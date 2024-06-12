ed9195ba1142bfbd4a9e087b61ba07bceed14f0b
Liwei Zou <zouliwei021@gmail.com>
2024-06-11 20:25:03 -0700

    Develop fundamental features for Dalle Clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Modal from "./components/Modal";
 
-function App() {
+const App = () => {
+  const surpriseOptions = [
+    'A blue ostrich eating melon',
+    'A matisse style shark on the telephone',
+    'A pineapple sunbathing on an island',
+  ]
+
+  const [images, setImages] = useState([])
+  const [inputValue, setInputValue] = useState('')
+  const [selectedImage, setSelectedImage] = useState(null)
+  const [modalOpen, setModalOpen] = useState(false)
+
+  const surpriseMe = async() => {
+    const randomValue = surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)]
+    setInputValue(randomValue)
+  }
+
+  const getImages = async() => {
+    setImages([])
+    if (!inputValue) {
+      return
+    }
+
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        message: inputValue
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }
+    try {
+      const response = await fetch('http://localhost:8000/images', options)
+      const data = await response.json()
+      console.log(data)
+      setImages(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const uploadImage = async(e) => {
+    const formData = new FormData()
+    formData.append('file', e.target.files[0])
+    setSelectedImage(e.target.files[0])
+    setModalOpen(true)
+    e.target.value = null
+
+    const options = {
+      method: 'POST',
+      body: formData,
+    }
+    try {
+      const response = await fetch('http://localhost:8000/upload', options)
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const generateVariations = async () => {
+    setImages([])
+    if (!selectedImage) {
+      setModalOpen(false)
+      return
+    }
+
+    const options = {
+      method: 'POST',
+    }
+    try {
+      const response = await fetch('http://localhost:8000/variations', options)
+      const data = await response.json()
+      console.log(data)
+      setImages(data)
+      setModalOpen(false)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="app">
+      <section className="search-section">
+        <p>Start with a detailed description
+          <span className="surprise" onClick={surpriseMe}>Surprise me</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="input-container">
+          <input
+            value={inputValue}
+            placeholder="An impressionist oil painting
+            of a sunflower in a purple vase..."
+            onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={e => e.key === 'Enter' && getImages()}
+          />
+          <button onClick={getImages}>Generate</button>
+        </div>
+        <p className="extra-info">
+          Or,
+          <span>
+            <label htmlFor="file"> upload an image </label>
+            <input onChange={uploadImage} id="file" type="file" accept="image/*" hidden/>
+          </span>
+          to edit.
+        </p>
+
+        {modalOpen &&
+          <div className="overlay">
+            <Modal
+              setModalOpen={setModalOpen}
+              setSelectedImage={setSelectedImage}
+              selectedImage={selectedImage}
+              generateVariations={generateVariations}
+            />
+          </div>}
+      </section>
+      <section className="image-section">
+        {images?.map((image, index) => (
+          <img key={index} src={image.url} alt="generated"/>
+        ))}
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Modal.js
@@ -0,0 +1,34 @@
+import { useState, useRef } from "react";
+
+const Modal = ({ setModalOpen, setSelectedImage, selectedImage, generateVariations }) => {
+  const [error, setError] = useState(null);
+  const ref = useRef(null);
+
+  const closeModal = () => {
+    setModalOpen(false)
+    setSelectedImage(null)
+  }
+
+  const checkSize = () => {
+    if (ref.current.width === 256 && ref.current.height === 256) {
+      generateVariations()
+    } else {
+      setError('Error: Choose 256 x 256 image')
+    }
+  }
+
+  return (
+    <div className="modal">
+      <div onClick={closeModal}>✖</div>
+      <div className="img-container">
+        {selectedImage &&
+          <img ref={ref} src={URL.createObjectURL(selectedImage)} alt="uploaded"/>}
+      </div>
+      <p>{error || "* Image must be 256 x 256"}</p>
+      {!error && <button onClick={checkSize}>Generate</button>}
+      {error && <button onClick={closeModal}>Close this and try again</button>}
+    </div>
+  )
+}
+
+export default Modal
